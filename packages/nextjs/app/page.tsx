cf203060ace3561905cd7c4cb05771d8245a0f0d
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2025-07-10 18:55:55 +0200

    updated landing page with more context

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { BeakerIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">MXNb-USDT LP Manager Bitso Hack</span>
           </h1>
           <div className="flex justify-center items-center space-x-2 flex-col">
             <p className="my-2 font-medium">Connected Address:</p>
@@ -23,41 +23,38 @@ const Home: NextPage = () => {
           </div>
 
           <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
+            This is a hackathon project built by{" "}
+            <a
+              href="https://x.com/0xdcota"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block hover:underline"
+            >
+              @0xdcota
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://x.com/iafhurtado"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block hover:underline"
+            >
+              @iafhurtado
+            </a>
           </p>
           <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
+            Using Scaffold-Eth 2.0, we built a liquidity manager front end for the MXNb-USDT LP.{" "}
           </p>
         </div>
 
         <div className="grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col md:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <BeakerIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
+                Tinker with our smart contract using the{" "}
+                <Link href="/liquidity-manager" passHref className="link">
+                  Liquidity Manager
                 </Link>{" "}
                 tab.
               </p>
